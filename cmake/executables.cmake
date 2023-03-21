43266e2eaa84a23ad7daf77dc74e82ddb4a49a6d
Anton Rybianov <apollo301269@gmail.com>
2023-03-21 20:14:01 +0300

    enh: remove unused code

--- cmake/executables.cmake
+++ cmake/executables.cmake
@@ -27,24 +27,3 @@ target_link_libraries(queue_client queue_service_proto CLI11::CLI11)
 
 add_executable(queue_service src/queue/service.cpp)
 target_link_libraries(queue_service queue_service_proto CLI11::CLI11 store)
-
-# ------------------------------------------------------------------------------
-# two-replica-queue
-# ------------------------------------------------------------------------------
-
-generate_proto(queue_service_proto2 src/two_replica_queue/queue_api.proto)
-
-add_executable(queue_client2 src/two_replica_queue/client.cpp)
-target_link_libraries(queue_client2 queue_service_proto2 CLI11::CLI11)
-
-add_executable(queue_service2 src/two_replica_queue/service.cpp)
-target_link_libraries(queue_service2 queue_service_proto2 CLI11::CLI11 store)
-
-# ------------------------------------------------------------------------------
-# replicated-queue
-# ------------------------------------------------------------------------------
-
-generate_proto(replicated_queue_proto src/queue_simulator/replicated_queue.proto)
-
-add_executable(replicated_queue src/queue_simulator/main.cpp)
-target_link_libraries(replicated_queue runtime_simulator replicated_queue_proto)
