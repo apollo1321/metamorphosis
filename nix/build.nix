8afbbc736fb3a65d4827de48481e0c687a9f37d6
Anton Rybianov <apollo301269@gmail.com>
2023-07-22 15:06:37 +0300

    add thesis

--- nix/build.nix
+++ nix/build.nix
@@ -1,20 +1,20 @@
 let
-  pkgs = import ./source.nix;
+  pkgs = (import ./source.nix).pkgs;
 in
-  with pkgs;
-  mkShell {
-    name = "Build environment";
-    packages = [ 
-      patch
-      cmake
-      ninja
-      llvmPackages_16.tools.libcxxClang
-      cacert
-    ];
-    shellHook = ''
-      export CXX=clang++
-      export CC=clang
-      export CXXFLAGS="-I${llvmPackages_16.libcxxabi.dev}/include/c++/v1/"
-      export CFLAGS="-I${llvmPackages_16.libcxxabi.dev}/include/c++/v1/"
-    '';
-  }
+with pkgs;
+mkShell {
+  name = "Build environment";
+  packages = [
+    patch
+    cmake
+    ninja
+    llvmPackages_16.tools.libcxxClang
+    cacert
+  ];
+  shellHook = ''
+    export CXX=clang++
+    export CC=clang
+    export CXXFLAGS="-I${llvmPackages_16.libcxxabi.dev}/include/c++/v1/"
+    export CFLAGS="-I${llvmPackages_16.libcxxabi.dev}/include/c++/v1/"
+  '';
+}
--- nix/test.nix
+++ nix/test.nix
@@ -1,12 +1,12 @@
 let
-  pkgs = import ./source.nix;
+  pkgs = (import ./source.nix).pkgs;
 in
-  with pkgs;
-  mkShell {
-    name = "Test environment";
-    packages = [ 
-      docker
-      python311
-      python311Packages.docker
-    ];
-  }
+with pkgs;
+mkShell {
+  name = "Test environment";
+  packages = [
+    docker
+    python311
+    python311Packages.docker
+  ];
+}
