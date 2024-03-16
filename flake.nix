6c8616e7b392f6c0379d691476b5c0f045ed4232
Anton Rybianov <apollo301269@gmail.com>
2024-03-16 21:23:55 +0300

    Move papers to separate repository

--- flake.nix
+++ flake.nix
@@ -4,12 +4,9 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
-
-    # Use old version of tectonic for biber version to be chosen correctly
-    nixpkgsTex.url = "github:NixOS/nixpkgs/nixos-22.11";
   };
 
-  outputs = { self, nixpkgs, flake-utils, nixpkgsTex }:
+  outputs = { self, nixpkgs, flake-utils }:
     let
       system = flake-utils.lib.system.x86_64-linux;
       pkgs = nixpkgs.legacyPackages.${system};
@@ -26,8 +23,6 @@
         export CXXFLAGS="-I${pkgs.llvmPackages_16.libcxxabi.dev}/include/c++/v1/"
         export CFLAGS="-I${pkgs.llvmPackages_16.libcxxabi.dev}/include/c++/v1/"
       '';
-
-      pkgsTex = nixpkgsTex.legacyPackages.${system};
     in
     {
       devShells.${system} = rec {
@@ -51,15 +46,6 @@
             docker
             python311
             python311Packages.docker
-          ];
-        };
-
-        latex = with pkgsTex; mkShell {
-          name = "LaTeX environment";
-          packages = [
-            python311Packages.pygments
-            which
-            tectonic
           ];
         };
       };
